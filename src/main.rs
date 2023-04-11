d5552a9a3fafdc5e5cd10891482cbe2521be944c
Alex Mediantsev <zencrazycat@gmail.com>
2023-04-11 09:52:00 +0300

    Implement elongation

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,7 @@ enum Direction {
 }
 
 struct Snake {
-    x: f64,
-    y: f64,
+    coordinates: Vec<(f64, f64)>,
     direction: Direction,
 }
 
@@ -37,30 +36,30 @@ fn main() {
         .exit_on_esc(true)
         .build()
         .unwrap();
-    window.set_ups(3);
+    window.set_ups(6);
 
-    let mut snake = Snake { x: 0.0, y: 0.0, direction: Direction::Right };
-    let mut food = Food {
-        x: 0.0,
-        y: 0.0,
-    };
+    let mut snake = Snake { coordinates: vec![(0.0, 0.0)], direction: Direction::Right };
+    let mut food = Food { x: 0.0, y: 0.0 };
     let (food_x, food_y) = generate_food_position();
     food.x = food_x;
     food.y = food_y;
 
-
     while let Some(event) = window.next() {
+        let (head_x, head_y) = *snake.coordinates.last().unwrap();
         if let Some(_) = event.render_args() {
             window.draw_2d(&event, |context, graphics, _| {
                 // Clear the window
                 clear([0.0, 0.0, 0.0, 1.0], graphics);
 
-                rectangle(
-                    [1.0, 1.0, 1.0, 1.0],
-                    [snake.x, snake.y, GRID_SIZE, GRID_SIZE],
-                    context.transform,
-                    graphics,
-                );
+                // Draw the snake
+                for &(x, y) in &snake.coordinates {
+                    rectangle(
+                        [0.0, 1.0, 0.0, 1.0],
+                        [x, y, GRID_SIZE, GRID_SIZE],
+                        context.transform,
+                        graphics,
+                    );
+                }
                 // Draw the food
                 rectangle(
                     [1.0, 0.0, 0.0, 1.0],
@@ -69,7 +68,8 @@ fn main() {
                     graphics,
                 );
 
-                if snake.x == food.x && snake.y == food.y {
+                // let (head_x, head_y) = *snake.coordinates.last().unwrap();
+                if head_x == food.x && head_y == food.y {
                     // Increase the snake's length and generate a new food position
                     let (new_food_x, new_food_y) = generate_food_position();
                     food.x = new_food_x;
@@ -79,28 +79,33 @@ fn main() {
         }
 
         match event.press_args() {
-            Some(Button::Keyboard(Key::Up)) => {
-                snake.direction = Direction::Up;
-            }
-            Some(Button::Keyboard(Key::Down)) => {
-                snake.direction = Direction::Down;
-            }
-            Some(Button::Keyboard(Key::Left)) => {
-                snake.direction = Direction::Left;
-            }
-            Some(Button::Keyboard(Key::Right)) => {
-                snake.direction = Direction::Right;
-            }
+            Some(Button::Keyboard(Key::Up)) => snake.direction = Direction::Up,
+            Some(Button::Keyboard(Key::Down)) => snake.direction = Direction::Down,
+            Some(Button::Keyboard(Key::Left)) => snake.direction = Direction::Left,
+            Some(Button::Keyboard(Key::Right)) => snake.direction = Direction::Right,
             _ => ()
         }
 
         if let Some(_) = event.update_args() {
-            match snake.direction {
-                Direction::Up => snake.y -= GRID_SIZE,
-                Direction::Down => snake.y += GRID_SIZE,
-                Direction::Left => snake.x -= GRID_SIZE,
-                Direction::Right => snake.x += GRID_SIZE,
+            let new_head = match snake.direction {
+                Direction::Up => (head_x, head_y - GRID_SIZE),
+                Direction::Down => (head_x, head_y + GRID_SIZE),
+                Direction::Left => (head_x - GRID_SIZE, head_y),
+                Direction::Right => (head_x + GRID_SIZE, head_y),
+            };
+
+            if new_head.0 == food.x && new_head.1 == food.y {
+                // Increase the snake's length and generate a new food position
+                let (new_food_x, new_food_y) = generate_food_position();
+                food.x = new_food_x;
+                food.y = new_food_y;
+            } else {
+                // Remove the tail
+                snake.coordinates.remove(0);
             }
+
+            // Add the new head position
+            snake.coordinates.push(new_head);
         }
     }
 }
