b7b7cea168b30e6163a98e75ef99df1f51860f6b
amediantsev <zencrazycat@gmail.com>
2023-04-14 10:08:41 +0300

    Add game lifecycle.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use piston_window::{clear, rectangle, Button, Key, PistonWindow, PressEvent, RenderEvent, WindowSettings};
+use piston_window::{clear, rectangle, Button, Key, PistonWindow, PressEvent, RenderEvent, WindowSettings, CharacterCache, G2d};
 use piston_window::{EventLoop, UpdateEvent};
+use piston_window::{text, Glyphs, TextureSettings, Transformed};
+use piston_window::{MouseCursorEvent, MouseButton};
+use piston_window::types::Color;
+use piston_window::math::Matrix2d;
 use rand::Rng;
-
 
 const GRID_SIZE: f64 = 20.0;
 const WINDOW_SIZE: f64 = 600.0;
+
+const BUTTON_WIDTH: f64 = 150.0;
+const BUTTON_HEIGHT: f64 = 50.0;
+const BUTTON_X: f64 = (WINDOW_SIZE - BUTTON_WIDTH) / 2.0;
+const BUTTON_Y: f64 = 350.0;
+const BUTTON_TEXT_SIZE: u32 = 24;
+const BRIGHT_GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
 
 enum Direction {
     Up,
@@ -13,22 +24,78 @@ enum Direction {
     Right,
 }
 
-struct Snake {
-    coordinates: Vec<(f64, f64)>,
-    direction: Direction,
-}
-
-struct Food {
+struct Cell {
     x: f64,
     y: f64,
+    color: Color,
 }
 
-fn generate_food_position() -> (f64, f64) {
+impl Cell {
+    fn draw(&self, transform: Matrix2d, graphics: &mut G2d) {
+        rectangle(
+            self.color,
+            [self.x, self.y, GRID_SIZE, GRID_SIZE],
+            transform,
+            graphics,
+        );
+    }
+}
+
+impl PartialEq for Cell {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+struct Snake {
+    coordinates: Vec<Cell>,
+    direction: Direction,
+    dead: bool,
+}
+
+impl Default for Snake {
+    fn default() -> Self {
+        Snake {
+            coordinates: vec![Cell { x: 0.0, y: 0.0, color: BRIGHT_GREEN }],
+            direction: Direction::Right,
+            dead: false,
+        }
+    }
+}
+
+impl Snake {
+    fn current_head(&self) -> &Cell { self.coordinates.last().unwrap() }
+
+    fn calculate_new_head(&self) -> Cell {
+        let position = self.current_head();
+        match self.direction {
+            Direction::Up => {
+                let new_y = position.y - GRID_SIZE;
+                Cell { x: position.x, y: if new_y < 0.0 { WINDOW_SIZE } else { new_y }, color: BRIGHT_GREEN }
+            }
+            Direction::Down => {
+                let new_y = position.y + GRID_SIZE;
+                Cell { x: position.x, y: if new_y > WINDOW_SIZE { 0.0 } else { new_y }, color: BRIGHT_GREEN }
+            }
+            Direction::Left => {
+                let new_x = position.x - GRID_SIZE;
+                Cell { x: if new_x < 0.0 { WINDOW_SIZE } else { new_x }, y: position.y, color: BRIGHT_GREEN }
+            }
+            Direction::Right => {
+                let new_x = position.x + GRID_SIZE;
+                Cell { x: if new_x > WINDOW_SIZE { 0.0 } else { new_x }, y: position.y, color: BRIGHT_GREEN }
+            }
+        }
+    }
+}
+
+fn generate_food() -> Cell {
     let mut rng = rand::thread_rng();
-    (
-        (rng.gen_range(0..(WINDOW_SIZE as u32 / GRID_SIZE as u32)) * GRID_SIZE as u32) as f64,
-        (rng.gen_range(0..(WINDOW_SIZE as u32 / GRID_SIZE as u32)) * GRID_SIZE as u32) as f64,
-    )
+    Cell {
+        x: (rng.gen_range(0..(WINDOW_SIZE as u32 / GRID_SIZE as u32)) * GRID_SIZE as u32) as f64,
+        y: (rng.gen_range(0..(WINDOW_SIZE as u32 / GRID_SIZE as u32)) * GRID_SIZE as u32) as f64,
+        color: [1.0, 0.0, 0.0, 1.0],
+    }
 }
 
 fn main() {
@@ -36,46 +103,78 @@ fn main() {
         .exit_on_esc(true)
         .build()
         .unwrap();
-    window.set_ups(6);
+    window.set_ups(7);
 
-    let mut snake = Snake { coordinates: vec![(0.0, 0.0)], direction: Direction::Right };
-    let mut food = Food { x: 0.0, y: 0.0 };
-    let (food_x, food_y) = generate_food_position();
-    food.x = food_x;
-    food.y = food_y;
+    let mut snake = Snake::default();
+    let mut food = generate_food();
+    let texture_context = window.create_texture_context();
+    let mut glyphs = Glyphs::new("/Library/Fonts/Arial Unicode.ttf", texture_context, TextureSettings::new()).unwrap();
+    let mut mouse_pos = [0.0, 0.0];
 
     while let Some(event) = window.next() {
-        let (head_x, head_y) = *snake.coordinates.last().unwrap();
         if let Some(_) = event.render_args() {
-            window.draw_2d(&event, |context, graphics, _| {
+            window.draw_2d(&event, |context, graphics, device| {
                 // Clear the window
                 clear([0.0, 0.0, 0.0, 1.0], graphics);
 
                 // Draw the snake
-                for &(x, y) in &snake.coordinates {
+                let head = snake.current_head();
+                for body_part in &snake.coordinates {
+                    body_part.draw(context.transform, graphics);
+                }
+                // Draw the food
+                food.draw(context.transform, graphics);
+
+                if snake.dead {
+                    let game_over_text = "GAME OVER";
+                    let game_over_text_style = text::Text::new_color([1.0, 1.0, 1.0, 1.0], 32);
+                    let game_over_text_width = glyphs.width(BUTTON_TEXT_SIZE, game_over_text).unwrap();
+                    let game_over_text_x = BUTTON_X - (BUTTON_WIDTH - game_over_text_width) / 2.0;
+                    let glyphs = &mut glyphs;
+
+                    game_over_text_style.draw(
+                        game_over_text,
+                        glyphs,
+                        &context.draw_state,
+                        context.transform.trans(game_over_text_x, 300.0),
+                        graphics,
+                    ).unwrap();
+
+                    // Draw the button
                     rectangle(
-                        [0.0, 1.0, 0.0, 1.0],
-                        [x, y, GRID_SIZE, GRID_SIZE],
+                        [1.0, 1.0, 1.0, 1.0],
+                        [BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT],
                         context.transform,
                         graphics,
                     );
-                }
-                // Draw the food
-                rectangle(
-                    [1.0, 0.0, 0.0, 1.0],
-                    [food.x, food.y, GRID_SIZE, GRID_SIZE],
-                    context.transform,
-                    graphics,
-                );
+                    let text = "RESTART";
+                    let text_style = text::Text::new_color([0.0, 0.0, 0.0, 1.0], BUTTON_TEXT_SIZE);
+                    let text_width = glyphs.width(BUTTON_TEXT_SIZE, text).unwrap();
+                    let text_x = BUTTON_X + (BUTTON_WIDTH - text_width) / 2.0;
+                    let text_y = BUTTON_Y + (BUTTON_HEIGHT - BUTTON_TEXT_SIZE as f64) / 2.0 + BUTTON_TEXT_SIZE as f64;
 
-                // let (head_x, head_y) = *snake.coordinates.last().unwrap();
-                if head_x == food.x && head_y == food.y {
+                    text_style.draw(
+                        text,
+                        glyphs,
+                        &context.draw_state,
+                        context.transform.trans(text_x, text_y),
+                        graphics,
+                    ).unwrap();
+
+
+                    // Update glyphs before rendering.
+                    glyphs.factory.encoder.flush(device);
+                }
+
+                if head.x == food.x && head.y == food.y {
                     // Increase the snake's length and generate a new food position
-                    let (new_food_x, new_food_y) = generate_food_position();
-                    food.x = new_food_x;
-                    food.y = new_food_y;
+                    food = generate_food();
                 }
             });
+        }
+
+        if let Some(pos) = event.mouse_cursor_args() {
+            mouse_pos = pos;
         }
 
         match event.press_args() {
@@ -83,29 +182,41 @@ fn main() {
             Some(Button::Keyboard(Key::Down)) => snake.direction = Direction::Down,
             Some(Button::Keyboard(Key::Left)) => snake.direction = Direction::Left,
             Some(Button::Keyboard(Key::Right)) => snake.direction = Direction::Right,
+            Some(Button::Mouse(MouseButton::Left)) => {
+                if snake.dead {
+                    let [mouse_x, mouse_y] = mouse_pos;
+                    if mouse_x >= BUTTON_X
+                        && mouse_x <= (BUTTON_X + BUTTON_WIDTH)
+                        && mouse_y >= BUTTON_Y
+                        && mouse_y <= (BUTTON_Y + BUTTON_HEIGHT)
+                    {
+                        // Restart the game
+                        snake = Snake::default();
+                        food = generate_food();
+                    }
+                }
+            }
             _ => ()
         }
 
         if let Some(_) = event.update_args() {
-            let new_head = match snake.direction {
-                Direction::Up => (head_x, head_y - GRID_SIZE),
-                Direction::Down => (head_x, head_y + GRID_SIZE),
-                Direction::Left => (head_x - GRID_SIZE, head_y),
-                Direction::Right => (head_x + GRID_SIZE, head_y),
-            };
+            let new_head = snake.calculate_new_head();
 
-            if new_head.0 == food.x && new_head.1 == food.y {
-                // Increase the snake's length and generate a new food position
-                let (new_food_x, new_food_y) = generate_food_position();
-                food.x = new_food_x;
-                food.y = new_food_y;
+            // Check if the snake's new head position encounters its own body
+            if snake.coordinates.contains(&new_head) {
+                snake.dead = true;
             } else {
-                // Remove the tail
-                snake.coordinates.remove(0);
-            }
+                if new_head.x == food.x && new_head.y == food.y {
+                    // Increase the snake's length and generate a new food position
+                    food = generate_food();
+                } else {
+                    // Remove the tail
+                    snake.coordinates.remove(0);
+                }
 
-            // Add the new head position
-            snake.coordinates.push(new_head);
+                // Add the new head position
+                snake.coordinates.push(new_head);
+            }
         }
     }
 }
